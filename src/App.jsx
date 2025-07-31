4f4f4cbd32ab1fcd60c16e4f77ef48bd59c8dfeb
Rudy <rudydanielcarballo@gmail.com>
2025-07-31 22:04:57 +0200

    Corrigiendo defectos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About";
+import { Home } from "./Pages/Home";
+import { About } from "./Pages/About";
 import { CubeTransition } from "./components/CubeTransition";
 import { Contact } from "./Pages/Contact";
 import { Services } from "./Pages/Services";
